e7fb19d8a67741626f1f1a67242c2a555f8812f3
zor009 <anzohra@outlook.com>
2019-10-19 23:52:58 -0400

    update

--- index.js
+++ index.js
@@ -360,6 +360,8 @@ function longestWord(sen) {
 
 function chunkArray(arr, len) {}
 
+//------------------------------------------------------------------------------------------------------------
+
 // CHALLENGE 3: FLATTEN ARRAY
 // Take an array of arrays and flatten to a single array
 // ex. [[1, 2], [3, 4], [5, 6], [7]] = [1, 2, 3, 4, 5, 6, 7]
