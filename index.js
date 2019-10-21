e01e1d968d315d46c2a8e5e851d61d76113c301f
zor009 <anzohra@outlook.com>
2019-10-21 10:41:34 -0400

    update

--- index.js
+++ index.js
@@ -377,6 +377,8 @@ function flattenArray(arrays) {}
 
 function isAnagram(str1, str2) {}
 
+//------------------------------------------------------------------------------------------------------------
+
 // CHALLENGE 5: LETTER CHANGES
 // Change every letter of the string to the one that follows it and capitalize the vowels
 // Z should turn to A
