2831d5190772f533cbbb9d6d3eeb34747711f4e6
zor009 <anzohra@outlook.com>
2020-01-08 00:19:37 -0500

    longest word solution 1 FOR LOOP

---
+++ longest_word.js
@@ -0,0 +1,34 @@
+// QUESTION: Return the length of the longest word in the provided sentence.
+// Your response should be a number.
+
+  findLongestWord("The quick brown fox jumped over the lazy dog");
+  function findLongestWord(str) {
+    // Step 1. Split the string into an array of strings
+    var strSplit = str.split(' ');
+    // var strSplit = "The quick brown fox jumped over the lazy dog".split(' ');
+    // var strSplit = ["The", "quick", "brown", "fox", "jumped", "over", "the", "lazy", "dog"];
+      
+    // Step 2. Initiate a variable that will hold the length of the longest word
+    var longestWord = 0;
+  
+    // Step 3. Create the FOR loop
+    for(var i = 0; i < strSplit.length; i++){
+      if(strSplit[i].length > longestWord){ // If strSplit[i].length is greater than the word it is compared with...
+      longestWord = strSplit[i].length; // ...then longestWord takes this new value
+       }
+    }
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+////////////////////////////////////////////////////////////////////////////////////////////
