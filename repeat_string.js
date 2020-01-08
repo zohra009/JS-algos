9b7943392b4f4749ae5614977de84fc00408fb7f
zor009 <anzohra@outlook.com>
2020-01-07 21:56:15 -0500

    repeat string algo

---
+++ repeat_string.js
@@ -0,0 +1,5 @@
+//repeat a given string a given set num of times 
+//function will take 2 params (str, num)
+// Step 1. Create an empty string that will host the repeated string
+// Step 2. Set the While loop with (times > 0) as the condition to check
+// Step 3. Return the repeated string
