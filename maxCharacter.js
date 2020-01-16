674f13c63ed79716597b39cf0fabb9a1baaa173f
zor009 <anzohra@outlook.com>
2020-01-15 20:52:36 -0500

    max charc algo

--- maxCharacter.js
+++ maxCharacter.js
@@ -2,4 +2,13 @@ function maxCharacter (str){
 const charMap = {};
 let max = 0;
 let maxChar = "";
+
+for(let char of str){
+    if(charMap[char]){
+        charMap++;
+    }else {
+        charMap[char] = 1;
+    }
+}
+
 }
