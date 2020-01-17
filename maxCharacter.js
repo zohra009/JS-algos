006839bd0c1d2629f3eb95c966767ac1216ddf5f
zor009 <anzohra@outlook.com>
2020-01-17 16:25:28 -0500

    max charc notes

--- maxCharacter.js
+++ maxCharacter.js
@@ -4,9 +4,12 @@ let max = 0;
 let maxChar = "";
 
 for(let char of str){
+    //if charMap[char] "that characrter (char) of charMap already exists"
     if(charMap[char]){
+        //add to the tally 
         charMap++;
     }else {
+        //otherwise put it in the charMap with a tally of 1
         charMap[char] = 1;
     }
 }
