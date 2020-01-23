a75b541cd81078424be6cafd87b44f025c8aaaa0
zor009 <anzohra@outlook.com>
2020-01-23 08:01:50 -0500

    anagram algo

---
+++ anagrams.js
@@ -0,0 +1,18 @@
+// --- Directions
+// Check to see if two provided strings are anagrams of eachother.
+// One string is an anagram of another if it uses the same characters
+// in the same quantity. Only consider characters, not spaces
+// or punctuation.  Consider capital letters to be the same as lower case
+// --- Examples
+//   anagrams('rail safety', 'fairy tales') --> True
+//   anagrams('RAIL! SAFETY!', 'fairy tales') --> True
+//   anagrams('Hi there', 'Bye there') --> False
+
+function anagrams(stringA, stringB) {
+    //sort the string in alphabetical order and lowerCase
+    //compare the lengths to yeild T/F (boolean)
+    let string1 = stringA.toLowerCase().split('').sort().join('')
+    let string2 = stringB.toLowerCase().split('').sort().join('')
+    return string1.length === string2.length
+}
+console.log(anagrams("Hello There"))
