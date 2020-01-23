03643f95a2cc2ca4bbbb83e00486b1b32bb2febd
zor009 <anzohra@outlook.com>
2020-01-22 21:47:19 -0500

    return the max char string

--- maxCharacter.js
+++ maxCharacter.js
@@ -8,9 +8,11 @@ module.exports = maxChar;
 
 function maxCharacter (str){
 const charMap = {};
+//these two are initialized AFTER first for loop is done
 let max = 0;
 let maxChar = "";
 
+//for of loop used to interate through arrays and strings
 for(let char of str){
     //if charMap[char] "that characrter (char) of charMap already exists"
     if(charMap[char]){
@@ -21,5 +23,15 @@ for(let char of str){
         charMap[char] = 1;
     }
 }
+//for in loop is used for interating through objects
+ for (let char in charMap){
+     //if char of obj > 0 
+     if(charMap[char] > max){
+         //set new max to charMap[char] of that particular char
+        max = charMap[char];
+        //and make maxChar (string) to equal that char
+        maxChar = char;
+     } 
+ }
 
 }
