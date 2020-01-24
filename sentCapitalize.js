6575bbf91b56314cf6c5838184356aaee8489bae
zor009 <anzohra@outlook.com>
2020-01-24 13:05:32 -0500

    sentence capitalize prompt

--- sentCapitalize.js
+++ sentCapitalize.js
@@ -7,6 +7,11 @@
 //   capitalize('a lazy fox') --> 'A Lazy Fox'
 //   capitalize('look, it is working!') --> 'Look, It Is Working!'
 
-function capitalize(str) {}
+function capitalize(str) {
+    //split str into arr of words 
+    //loop through the arr & take first letter of every word and .toUpperCase()
+    //return the phrase
+    
+}
 
 module.exports = capitalize;
