42c3600e7d645f9f6cb5eb57c63ded61ac8f8229
zor009 <anzohra@outlook.com>
2020-02-07 16:16:13 -0500

    repeat str solun

--- repeatString.js
+++ repeatString.js
@@ -1,3 +1,9 @@
 function repeatStringNumTimes(string, times) {
+   //use .repeat method to check if times > 0 ; then repeat string
+   if( times > 0){
+       return string.repeat(times)
+   } else {
+       return ""
+   }
 
 }
